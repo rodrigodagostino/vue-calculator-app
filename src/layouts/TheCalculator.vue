d8d0e62be02bc7139d1f53cc9aae50c880378772
Rodrigo D’Agostino <rodrigo.dagostino@protonmail.com>
2021-06-21 18:05:48 -0300

    Adjusted the existing functions and created a few new ones to improve the data handling, added a new element to display the current operation (to display more precise information different than the one in history), improved the naming of most variables and functions, moved the history log to the new «HistoryPanel» component, and made a few adjustments to the styling.

--- src/layouts/TheCalculator.vue
+++ src/layouts/TheCalculator.vue
@@ -1,139 +1,196 @@
 <template>
 	<section class="calculator">
-		<ul class="history">
-			<li v-for="log in history" :key="log">{{ log }}</li>
-		</ul>
-		<div class="display">{{ currDisplay || 0 }}</div>
+		<button type="button" @click="toggleHistoryVisibility" class="button history">
+			<i class="fas fa-history" />
+		</button>
+		<p class="current-operation">{{ currOperation }}</p>
+		<div class="display">{{ currOperand || 0 }}</div>
 		<button type="button" @click="clear" class="button helper">C</button>
 		<button type="button" @click="percent" class="button helper">%</button>
 		<button type="button" @click="backwardDelete" class="button helper">⌫</button>
 		<button type="button" @click="divide" class="button operator">÷</button>
-		<button type="button" @click="attach(7)" class="button">7</button>
-		<button type="button" @click="attach(8)" class="button">8</button>
-		<button type="button" @click="attach(9)" class="button">9</button>
+		<button type="button" @click="attachToCurrOperand(7)" class="button">7</button>
+		<button type="button" @click="attachToCurrOperand(8)" class="button">8</button>
+		<button type="button" @click="attachToCurrOperand(9)" class="button">9</button>
 		<button type="button" @click="multiply" class="button operator">×</button>
-		<button type="button" @click="attach(4)" class="button">4</button>
-		<button type="button" @click="attach(5)" class="button">5</button>
-		<button type="button" @click="attach(6)" class="button">6</button>
+		<button type="button" @click="attachToCurrOperand(4)" class="button">4</button>
+		<button type="button" @click="attachToCurrOperand(5)" class="button">5</button>
+		<button type="button" @click="attachToCurrOperand(6)" class="button">6</button>
 		<button type="button" @click="subtract" class="button operator">-</button>
-		<button type="button" @click="attach(1)" class="button">1</button>
-		<button type="button" @click="attach(2)" class="button">2</button>
-		<button type="button" @click="attach(3)" class="button">3</button>
+		<button type="button" @click="attachToCurrOperand(1)" class="button">1</button>
+		<button type="button" @click="attachToCurrOperand(2)" class="button">2</button>
+		<button type="button" @click="attachToCurrOperand(3)" class="button">3</button>
 		<button type="button" @click="add" class="button operator">+</button>
 		<button type="button" @click="invert" class="button">±</button>
-		<button type="button" @click="attach(0)" class="button">0</button>
-		<button type="button" @click="attach('.')" class="button">.</button>
+		<button type="button" @click="attachToCurrOperand(0)" class="button">0</button>
+		<button type="button" @click="attachToCurrOperand('.')" class="button">.</button>
 		<button type="button" @click="equal" class="button operator">=</button>
+		<HistoryPanel :history="history" :class="{ 'is-visible': isHistoryVisible }" />
 	</section>
 </template>
 
 <script>
+import HistoryPanel from '../components/HistoryPanel.vue'
 import { ref, reactive } from 'vue'
 
 export default {
 	name: 'TheCalculator',
+	components: {
+		HistoryPanel,
+	},
 	setup() {
 		const history = reactive( [] )
-		const prevDisplay = ref( null )
-		const currDisplay = ref( '' )
-		let operator = null
+		const isHistoryVisible = ref( false )
+		const currOperation = ref( '' )
+		const prevOperand = ref( null )
+		const currOperand = ref( '' )
+		let currOperator = null
 		let hasActiveOperator = false
 
-		const clear = () => currDisplay.value = ''
-
-		const percent = () => currDisplay.value = +currDisplay.value / 1000
-
-		const backwardDelete = () => currDisplay.value = currDisplay.value.slice( 0, -1 )
-
-		const invert = () => {
-			currDisplay.value =
-				currDisplay.value.charAt( 0 ) !== '-'
-					? `-${ currDisplay.value }`
-					: currDisplay.value.slice( 1 )
+		const toggleHistoryVisibility = () => {
+			console.log( 'Working' )
+			isHistoryVisible.value = !isHistoryVisible.value
 		}
 
-		const attach = character => {
-			// Prevent multiple periods.
-			if ( character === '.' && currDisplay.value.indexOf( '.' ) !== -1 ) return
-			// Prevent multiple zeros at the beginning.
-			if ( character === 0 && currDisplay.value === 0 ) return
-			// Prevent absence of zero on initial period.
-			if ( character === '.' && currDisplay.value === '' ) currDisplay.value = 0
+		const clear = () => currOperand.value = ''
+
+		const backwardDelete = () => currOperand.value = currOperand.value.slice( 0, -1 )
+
+		const invert = () => {
+			currOperand.value =
+				currOperand.value.charAt( 0 ) !== '-'
+					? `-${ currOperand.value }`
+					: currOperand.value.slice( 1 )
+		}
+
+		const operate = {
+			'+': ( a, b ) => `${ ( +a + +b ).toFixed( 2 ) }`,
+			'-': ( a, b ) => `${ ( +a - +b ).toFixed( 2 ) }`,
+			'×': ( a, b ) => `${ ( +a * +b ).toFixed( 2 ) }`,
+			'÷': ( a, b ) => `${ ( +a / +b ).toFixed( 2 ) }`,
+			'%': a => `${ +a / 100 }`,
+		}
+
+		const attachToCurrOperation = character => {
+			// If an operation has finished, reset the current operation.
+			if (
+				currOperation.value &&
+				currOperation.value[ currOperation.value.length - 1 ] === '='
+			) {
+				currOperation.value = ''
+			}
+
+			if (
+				currOperation.value === '' &&
+				( character === '+' || character === '-' || character === '×' || character === '÷' )
+			) {
+				currOperation.value += currOperand.value + character
+			} else {
+				currOperation.value += character
+			}
+		}
+
+		const attachToCurrOperand = character => {
+			// Clean up if this is the first insertion after an operator.
 			if ( hasActiveOperator ) {
 				clear()
 				hasActiveOperator = false
 			}
-			currDisplay.value = `${ currDisplay.value }${ character }`
+			// Prevent multiple periods.
+			if ( character === '.' && currOperand.value.indexOf( '.' ) !== -1 ) return
+			// Prevent multiple zeros at the beginning.
+			if ( character === 0 && currOperand.value === 0 ) return
+			// Prevent absence of zero on initial period.
+			if ( character === '.' && currOperand.value === '' ) {
+				attachToCurrOperation( 0 )
+				currOperand.value = 0
+			}
+			currOperand.value = `${ currOperand.value }${ character }`
+			attachToCurrOperation( character )
 		}
 
-		const setPreviousDisplay = () => {
-			prevDisplay.value = currDisplay.value
-			hasActiveOperator = true
+		const setPrevOperand = () => {
+			prevOperand.value = currOperand.value
+		}
+
+		const setCurrOperand = () => {
+			if ( currOperator === '%' ) {
+				currOperand.value = operate[ currOperator ]( currOperand.value )
+			} else {
+				currOperand.value = operate[ currOperator ]( prevOperand.value, currOperand.value )
+			}
+		}
+
+		const attachToHistory = () => {
+			if ( currOperator === '%' ) {
+				history.push( currOperation.value + operate[ currOperator ]( currOperand.value ) )
+			} else {
+				history.push(
+					currOperation.value +
+						operate[ currOperator ]( prevOperand.value, currOperand.value ),
+				)
+			}
 		}
 
 		const add = () => {
-			operator = 'addition'
-			setPreviousDisplay()
+			currOperator = '+'
+			hasActiveOperator = true
+			attachToCurrOperation( '+' )
+			setPrevOperand()
 		}
 
 		const subtract = () => {
-			operator = 'subtraction'
-			setPreviousDisplay()
+			currOperator = '-'
+			hasActiveOperator = true
+			attachToCurrOperation( '-' )
+			setPrevOperand()
 		}
 
 		const multiply = () => {
-			operator = 'multiplication'
-			setPreviousDisplay()
+			currOperator = '×'
+			hasActiveOperator = true
+			attachToCurrOperation( '×' )
+			setPrevOperand()
 		}
 
 		const divide = () => {
-			operator = 'division'
-			setPreviousDisplay()
+			currOperator = '÷'
+			hasActiveOperator = true
+			attachToCurrOperation( '÷' )
+			setPrevOperand()
 		}
 
-		const log = {
-			'+': ( a, b ) => `${ a } + ${ b } = ${ +a + +b }`,
-			'-': ( a, b ) => `${ a } + ${ b } = ${ +a - +b }`,
-			'*': ( a, b ) => `${ a } × ${ b } = ${ +a * +b }`,
-			'/': ( a, b ) => `${ a } ÷ ${ b } = ${ +a / +b }`,
+		const percent = () => {
+			currOperator = '%'
+			// Will allow for a new operation to start if a digit is pressed instead of an operator.
+			hasActiveOperator = true
+			attachToCurrOperation( '%=' )
+			attachToHistory()
+			setCurrOperand()
+			prevOperand.value = null
 		}
 
 		const equal = () => {
-			switch ( operator ) {
-				case 'addition':
-					history.push( log[ '+' ]( prevDisplay.value, currDisplay.value ) )
-					currDisplay.value = `${ +prevDisplay.value + +currDisplay.value }`
-					break
-				case 'subtraction':
-					history.push( log[ '-' ]( prevDisplay.value, currDisplay.value ) )
-					currDisplay.value = `${ +prevDisplay.value - +currDisplay.value }`
-					break
-				case 'multiplication':
-					history.push( log[ '*' ]( prevDisplay.value, currDisplay.value ) )
-					currDisplay.value = `${ +prevDisplay.value * +currDisplay.value }`
-					break
-				case 'division':
-					history.push( log[ '/' ]( prevDisplay.value, currDisplay.value ) )
-					currDisplay.value = `${ +prevDisplay.value / +currDisplay.value }`
-					break
-				default:
-					break
-			}
-			prevDisplay.value = null
-			// Will allow for a new operation to start
-			// if a digit is pressed instead of an operator.
+			attachToCurrOperation( '=' )
+			attachToHistory()
+			setCurrOperand()
+			prevOperand.value = null
+			// Will allow for a new operation to start if a digit is pressed instead of an operator.
 			hasActiveOperator = true
 		}
 
 		return {
 			history,
-			prevDisplay,
-			currDisplay,
+			isHistoryVisible,
+			toggleHistoryVisibility,
+			currOperation,
+			prevOperand,
+			currOperand,
 			clear,
 			percent,
 			backwardDelete,
 			invert,
-			attach,
+			attachToCurrOperand,
 			add,
 			subtract,
 			multiply,
@@ -149,6 +206,7 @@ export default {
 .calculator {
 	font-size: 1.5rem;
 	color: var(--white);
+	background-color: var(--gray-950);
 	width: 22rem;
 	max-width: 100%;
 	padding: 1rem;
@@ -156,18 +214,22 @@ export default {
 	display: grid;
 	grid-template-columns: repeat(4, 1fr);
 	grid-gap: 0.25rem;
-	background-color: var(--gray-950);
+	position: relative;
+	overflow: visible;
 }
 
-.history {
+.current-operation {
 	grid-column: 1/5;
-	list-style: none;
+	color: var(--gray-400);
+	text-align: right;
+	padding: 0 1rem;
+	min-height: 1.75rem;
 }
 
 .display {
 	font-size: 3rem;
 	text-align: right;
-	padding: 1.5rem;
+	padding: 0.5rem 1rem 1.5rem;
 	grid-column: 1/5;
 }
 
@@ -178,6 +240,7 @@ export default {
 	background-color: var(--gray-900);
 	transition: background-color 0.32s ease, outline 0.32s ease;
 	outline: 2px solid transparent;
+	cursor: pointer;
 
 	&:focus {
 		outline: 2px solid var(--white);
@@ -186,6 +249,11 @@ export default {
 	&:hover {
 		background-color: var(--gray-800);
 	}
+}
+
+.history {
+	grid-column: 1/2;
+	background-color: transparent;
 }
 
 .operator {
