d47c0fb85e4ae05743f8abd781b0ffefba3bc4fa
Rodrigo D’Agostino <rodrigo.dagostino@gmail.com>
2021-11-06 15:08:22 -0300

    Switch project indentation to 2 spaces

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-	<main class="site-main">
-		<div class="container">
-			<TheCalculator />
-		</div>
-	</main>
-	<footer class="site-footer">
-		<div class="container">
-			<a href="https://github.com/rodrigodagostino/vue-calculator-app" target="_blank">
-				Made with <img src="@/assets/vue-logo.png" alt="Vue.js logo" /> by Rodrigo D’Agostino
-			</a>
-		</div>
-	</footer>
+  <main class="site-main">
+    <div class="container">
+      <TheCalculator />
+    </div>
+  </main>
+  <footer class="site-footer">
+    <div class="container">
+      <a href="https://github.com/rodrigodagostino/vue-calculator-app" target="_blank">
+        Made with <img src="@/assets/vue-logo.png" alt="Vue.js logo" /> by Rodrigo D’Agostino
+      </a>
+    </div>
+  </footer>
 </template>
 
 <script setup>
@@ -19,72 +19,72 @@ import TheCalculator from '@/layouts/TheCalculator.vue'
 
 <style lang="scss">
 /**
- * Variables
- */
+* Variables
+*/
 :root {
-	--font-main: 'Rubik', Avenir, Helvetica, Arial, sans-serif;
-	--color-main--lightest: #c7d2fe;
-	--color-main--lighter: #a5b4fc;
-	--color-main--light: #818cf8;
-	--color-main: #6366f1;
-	--color-main--dark: #4f46e5;
-	--color-main--darker: #4338ca;
-	--color-main--darkest: #3730a3;
+  --font-main: 'Rubik', Avenir, Helvetica, Arial, sans-serif;
+  --color-main--lightest: #c7d2fe;
+  --color-main--lighter: #a5b4fc;
+  --color-main--light: #818cf8;
+  --color-main: #6366f1;
+  --color-main--dark: #4f46e5;
+  --color-main--darker: #4338ca;
+  --color-main--darkest: #3730a3;
 
-	--teal-050: #f0fdfa;
-	--teal-100: #ccfbf1;
-	--teal-200: #99f6e4;
-	--teal-300: #5eead4;
-	--teal-400: #2dd4bf;
-	--teal-500: #14b8a6;
-	--teal-600: #0d9488;
-	--teal-700: #0f766e;
-	--teal-800: #115e59;
-	--teal-900: #134e4a;
+  --teal-050: #f0fdfa;
+  --teal-100: #ccfbf1;
+  --teal-200: #99f6e4;
+  --teal-300: #5eead4;
+  --teal-400: #2dd4bf;
+  --teal-500: #14b8a6;
+  --teal-600: #0d9488;
+  --teal-700: #0f766e;
+  --teal-800: #115e59;
+  --teal-900: #134e4a;
 
-	--purple-050: #faf5ff;
-	--purple-100: #f3e8ff;
-	--purple-200: #e9d5ff;
-	--purple-300: #d8b4fe;
-	--purple-400: #c084fc;
-	--purple-500: #a855f7;
-	--purple-600: #9333ea;
-	--purple-700: #7e22ce;
-	--purple-800: #6b21a8;
-	--purple-900: #581c87;
+  --purple-050: #faf5ff;
+  --purple-100: #f3e8ff;
+  --purple-200: #e9d5ff;
+  --purple-300: #d8b4fe;
+  --purple-400: #c084fc;
+  --purple-500: #a855f7;
+  --purple-600: #9333ea;
+  --purple-700: #7e22ce;
+  --purple-800: #6b21a8;
+  --purple-900: #581c87;
 
-	--white: #f3f3f5;
-	--gray-050: #ededf0;
-	--gray-100: #e1e1e6;
-	--gray-150: #d2d2da;
-	--gray-200: #c3c3ce;
-	--gray-300: #a4a6b5;
-	--gray-400: #86889d;
-	--gray-500: #686a84;
-	--gray-600: #585a70;
-	--gray-650: #515266;
-	--gray-700: #494a5c;
-	--gray-750: #414253;
-	--gray-800: #393a49;
-	--gray-850: #31323f;
-	--gray-900: #2a2a35;
-	--gray-950: #22222b;
-	--black: #15151a;
+  --white: #f3f3f5;
+  --gray-050: #ededf0;
+  --gray-100: #e1e1e6;
+  --gray-150: #d2d2da;
+  --gray-200: #c3c3ce;
+  --gray-300: #a4a6b5;
+  --gray-400: #86889d;
+  --gray-500: #686a84;
+  --gray-600: #585a70;
+  --gray-650: #515266;
+  --gray-700: #494a5c;
+  --gray-750: #414253;
+  --gray-800: #393a49;
+  --gray-850: #31323f;
+  --gray-900: #2a2a35;
+  --gray-950: #22222b;
+  --black: #15151a;
 }
 
 /**
- * Reset styles.
- */
+* Reset styles.
+*/
 html {
-	box-sizing: border-box;
-	height: 100%;
+  box-sizing: border-box;
+  height: 100%;
 }
 
 *,
 *::before,
 *::after {
-	/* Inherit box-sizing to make it easier to change the property for components that leverage other behavior; see http://css-tricks.com/inheriting-box-sizing-probably-slightly-better-best-practice/ */
-	box-sizing: inherit;
+  /* Inherit box-sizing to make it easier to change the property for components that leverage other behavior; see http://css-tricks.com/inheriting-box-sizing-probably-slightly-better-best-practice/ */
+  box-sizing: inherit;
 }
 
 /* Reset margins and paddings on most elements */
@@ -103,18 +103,18 @@ pre,
 blockquote,
 figure,
 hr {
-	margin: 0;
-	padding: 0;
+  margin: 0;
+  padding: 0;
 }
 
 a {
-	color: var(--white);
-	transition: outline 0.32s ease;
-	outline: 1px dotted transparent;
+  color: var(--white);
+  transition: outline 0.32s ease;
+  outline: 1px dotted transparent;
 }
 
 a:focus {
-	outline: 1px dotted var(--white);
+  outline: 1px dotted var(--white);
 }
 
 /* Reset forms and buttons */
@@ -122,26 +122,26 @@ input,
 textarea,
 select,
 button {
-	color: inherit;
-	font: inherit;
-	letter-spacing: inherit;
+  color: inherit;
+  font: inherit;
+  letter-spacing: inherit;
 }
 
 input,
 textarea,
 button {
-	border: 1px solid gray;
-	min-width: 0;
+  border: 1px solid gray;
+  min-width: 0;
 }
 
 button {
-	border-radius: 0;
-	padding: 0.75em 1em;
-	background-color: transparent;
+  border-radius: 0;
+  padding: 0.75em 1em;
+  background-color: transparent;
 }
 
 button * {
-	pointer-events: none;
+  pointer-events: none;
 }
 
 /* Easy responsive for media elements */
@@ -150,131 +150,131 @@ iframe,
 img,
 object,
 video {
-	display: block;
-	max-width: 100%;
+  display: block;
+  max-width: 100%;
 }
 
 /* Useful table styles */
 table {
-	table-layout: fixed;
-	width: 100%;
+  table-layout: fixed;
+  width: 100%;
 }
 
 /**
- * Base styles.
- */
+* Base styles.
+*/
 body {
-	font-family: var(--font-main);
-	font-size: 1rem;
-	color: var(--gray-600);
-	background-color: var(--gray-100);
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	height: 100%;
-	display: flex;
-	justify-content: center;
+  font-family: var(--font-main);
+  font-size: 1rem;
+  color: var(--gray-600);
+  background-color: var(--gray-100);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+  display: flex;
+  justify-content: center;
 }
 
 .site {
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
-	&::before {
-		content: '';
-		height: 2rem;
-	}
+  &::before {
+    content: '';
+    height: 2rem;
+  }
 }
 
 .container {
-	max-width: 52rem;
-	margin: 0 auto;
-	padding: 1rem;
-	position: relative;
+  max-width: 52rem;
+  margin: 0 auto;
+  padding: 1rem;
+  position: relative;
 }
 
 .site-footer {
-	font-size: 0.875rem;
-	text-align: center;
+  font-size: 0.875rem;
+  text-align: center;
 }
 
 .site-footer .container {
-	display: flex;
-	justify-content: center;
-	padding: 1rem 0;
+  display: flex;
+  justify-content: center;
+  padding: 1rem 0;
 }
 
 .site-footer a {
-	color: var(--gray-400);
-	text-decoration: none;
-	transition: color 0.24s ease;
+  color: var(--gray-400);
+  text-decoration: none;
+  transition: color 0.24s ease;
 }
 
 .site-footer a:focus,
 .site-footer a:hover {
-	color: var(--gray-600);
+  color: var(--gray-600);
 }
 
 .site-footer img {
-	height: 1.25rem;
-	width: auto;
-	vertical-align: middle;
-	margin: 0 0.25rem;
-	display: inline-block;
+  height: 1.25rem;
+  width: auto;
+  vertical-align: middle;
+  margin: 0 0.25rem;
+  display: inline-block;
 }
 
 .fade-slide-up-enter-active {
-	animation: fade-slide-up 0.32s ease;
+  animation: fade-slide-up 0.32s ease;
 }
 
 .fade-slide-up-leave-active {
-	animation: fade-slide-up 0.32s ease reverse;
+  animation: fade-slide-up 0.32s ease reverse;
 }
 
 @keyframes fade-slide-up {
-	from {
-		opacity: 0;
-		transform: translate3d(0, 2rem, 0);
-	}
-	to {
-		opacity: 1;
-		transform: translate3d(0, 0, 0);
-	}
+  from {
+    opacity: 0;
+    transform: translate3d(0, 2rem, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
 }
 
 .fade-slide-right-enter-active {
-	animation: fade-slide-right 0.32s ease;
+  animation: fade-slide-right 0.32s ease;
 }
 
 .fade-slide-right-leave-active {
-	animation: fade-slide-right 0.32s ease reverse;
+  animation: fade-slide-right 0.32s ease reverse;
 }
 
 @keyframes fade-slide-right {
-	from {
-		opacity: 0;
-		transform: translate3d(-2rem, 0, 0);
-	}
-	to {
-		opacity: 1;
-		transform: translate3d(0, 0, 0);
-	}
+  from {
+    opacity: 0;
+    transform: translate3d(-2rem, 0, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
 }
 
 .fade-enter-active {
-	animation: fade 0.32s ease;
+  animation: fade 0.32s ease;
 }
 
 .fade-leave-active {
-	animation: fade 0.32s ease reverse;
+  animation: fade 0.32s ease reverse;
 }
 
 @keyframes fade {
-	from {
-		opacity: 0;
-	}
-	to {
-		opacity: 1;
-	}
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 </style>
--- src/layouts/TheCalculator.vue
+++ src/layouts/TheCalculator.vue
@@ -1,32 +1,32 @@
 <template>
-	<section class="calculator">
-		<button type="button" @click="toggleHistoryVisibility" class="button history">
-			<i class="fas fa-history" />
-		</button>
-		<p class="current-operation">{{ currOperation }}</p>
-		<div class="display">{{ currOperand || 0 }}</div>
-		<button class="button helper" id="button-delete" @click="clear">C</button>
-		<button class="button helper" id="button-percent" @click="percent">%</button>
-		<button class="button helper" id="button-clear-entry" @click="clearEntry">⌫</button>
-		<button class="button operator" id="button-divide" @click="divide">÷</button>
-		<button class="button" id="button-seven" @click="attachToCurrOperand(7)">7</button>
-		<button class="button" id="button-eight" @click="attachToCurrOperand(8)">8</button>
-		<button class="button" id="button-nine" @click="attachToCurrOperand(9)">9</button>
-		<button class="button operator" id="button-multiply" @click="multiply">×</button>
-		<button class="button" id="button-four" @click="attachToCurrOperand(4)">4</button>
-		<button class="button" id="button-five" @click="attachToCurrOperand(5)">5</button>
-		<button class="button" id="button-six" @click="attachToCurrOperand(6)">6</button>
-		<button class="button operator" id="button-subtract" @click="subtract">-</button>
-		<button class="button" id="button-one" @click="attachToCurrOperand(1)">1</button>
-		<button class="button" id="button-two" @click="attachToCurrOperand(2)">2</button>
-		<button class="button" id="button-three" @click="attachToCurrOperand(3)">3</button>
-		<button class="button operator" id="button-add" @click="add">+</button>
-		<button class="button" id="button-one" @click="negate">±</button>
-		<button class="button" id="button-zero" @click="attachToCurrOperand(0)">0</button>
-		<button class="button" id="button-period" @click="attachToCurrOperand('.')">.</button>
-		<button class="button operator" id="button-equal" @click="equal">=</button>
-		<HistoryPanel :history="history" :class="{ 'is-visible': isHistoryVisible }" />
-	</section>
+  <section class="calculator">
+    <button type="button" @click="toggleHistoryVisibility" class="button history">
+      <i class="fas fa-history" />
+    </button>
+    <p class="current-operation">{{ currOperation }}</p>
+    <div class="display">{{ currOperand || 0 }}</div>
+    <button class="button helper" id="button-delete" @click="clear">C</button>
+    <button class="button helper" id="button-percent" @click="percent">%</button>
+    <button class="button helper" id="button-clear-entry" @click="clearEntry">⌫</button>
+    <button class="button operator" id="button-divide" @click="divide">÷</button>
+    <button class="button" id="button-seven" @click="attachToCurrOperand(7)">7</button>
+    <button class="button" id="button-eight" @click="attachToCurrOperand(8)">8</button>
+    <button class="button" id="button-nine" @click="attachToCurrOperand(9)">9</button>
+    <button class="button operator" id="button-multiply" @click="multiply">×</button>
+    <button class="button" id="button-four" @click="attachToCurrOperand(4)">4</button>
+    <button class="button" id="button-five" @click="attachToCurrOperand(5)">5</button>
+    <button class="button" id="button-six" @click="attachToCurrOperand(6)">6</button>
+    <button class="button operator" id="button-subtract" @click="subtract">-</button>
+    <button class="button" id="button-one" @click="attachToCurrOperand(1)">1</button>
+    <button class="button" id="button-two" @click="attachToCurrOperand(2)">2</button>
+    <button class="button" id="button-three" @click="attachToCurrOperand(3)">3</button>
+    <button class="button operator" id="button-add" @click="add">+</button>
+    <button class="button" id="button-one" @click="negate">±</button>
+    <button class="button" id="button-zero" @click="attachToCurrOperand(0)">0</button>
+    <button class="button" id="button-period" @click="attachToCurrOperand('.')">.</button>
+    <button class="button operator" id="button-equal" @click="equal">=</button>
+    <HistoryPanel :history="history" :class="{ 'is-visible': isHistoryVisible }" />
+  </section>
 </template>
 
 <script setup>
@@ -42,7 +42,7 @@ let currOperator = null
 let hasActiveOperator = false
 
 const toggleHistoryVisibility = () => {
-	isHistoryVisible.value = !isHistoryVisible.value
+  isHistoryVisible.value = !isHistoryVisible.value
 }
 
 const clear = () => currOperand.value = ''
@@ -50,251 +50,251 @@ const clear = () => currOperand.value = ''
 const clearEntry = () => currOperand.value = currOperand.value.slice( 0, -1 )
 
 const negate = () => {
-	currOperand.value = currOperand.value.charAt( 0 ) !== '-' ? `-${ currOperand.value }` : currOperand.value.slice( 1 )
+  currOperand.value = currOperand.value.charAt( 0 ) !== '-' ? `-${ currOperand.value }` : currOperand.value.slice( 1 )
 }
 
 const operate = {
-	'+': ( a, b ) => `${ +a + +b }`,
-	'-': ( a, b ) => `${ +a - +b }`,
-	'×': ( a, b ) => `${ +a * +b }`,
-	'÷': ( a, b ) => `${ +a / +b }`,
-	'%': a => `${ +a / 100 }`,
+  '+': ( a, b ) => `${ +a + +b }`,
+  '-': ( a, b ) => `${ +a - +b }`,
+  '×': ( a, b ) => `${ +a * +b }`,
+  '÷': ( a, b ) => `${ +a / +b }`,
+  '%': a => `${ +a / 100 }`,
 }
 
 const attachToCurrOperation = character => {
-	// If an operation has finished, reset the current operation.
-	if ( currOperation.value && currOperation.value[ currOperation.value.length - 1 ] === '=' ) {
-		currOperation.value = ''
-	}
+  // If an operation has finished, reset the current operation.
+  if ( currOperation.value && currOperation.value[ currOperation.value.length - 1 ] === '=' ) {
+    currOperation.value = ''
+  }
 
-	if (
-		currOperation.value === '' &&
+  if (
+    currOperation.value === '' &&
 		( character === '+' || character === '-' || character === '×' || character === '÷' )
-	) {
-		currOperation.value += currOperand.value + character
-	} else {
-		currOperation.value += character
-	}
+  ) {
+    currOperation.value += currOperand.value + character
+  } else {
+    currOperation.value += character
+  }
 }
 
 const attachToCurrOperand = character => {
-	// Clean up if this is the first insertion after an operator.
-	if ( hasActiveOperator ) {
-		clear()
-		hasActiveOperator = false
-	}
-	// Prevent multiple periods.
-	if ( character === '.' && currOperand.value.indexOf( '.' ) !== -1 ) return
-	// Prevent multiple zeros at the beginning.
-	if ( character === 0 && currOperand.value === 0 ) return
-	// Prevent absence of zero on initial period.
-	if ( character === '.' && currOperand.value === '' ) {
-		attachToCurrOperation( 0 )
-		currOperand.value = 0
-	}
-	currOperand.value = `${ currOperand.value }${ character }`
-	attachToCurrOperation( character )
+  // Clean up if this is the first insertion after an operator.
+  if ( hasActiveOperator ) {
+    clear()
+    hasActiveOperator = false
+  }
+  // Prevent multiple periods.
+  if ( character === '.' && currOperand.value.indexOf( '.' ) !== -1 ) return
+  // Prevent multiple zeros at the beginning.
+  if ( character === 0 && currOperand.value === 0 ) return
+  // Prevent absence of zero on initial period.
+  if ( character === '.' && currOperand.value === '' ) {
+    attachToCurrOperation( 0 )
+    currOperand.value = 0
+  }
+  currOperand.value = `${ currOperand.value }${ character }`
+  attachToCurrOperation( character )
 }
 
 const setPrevOperand = () => {
-	prevOperand.value = currOperand.value
+  prevOperand.value = currOperand.value
 }
 
 const setCurrOperand = () => {
-	if ( currOperator === '%' ) {
-		currOperand.value = operate[ currOperator ]( currOperand.value )
-	} else {
-		currOperand.value = operate[ currOperator ]( prevOperand.value, currOperand.value )
-	}
+  if ( currOperator === '%' ) {
+    currOperand.value = operate[ currOperator ]( currOperand.value )
+  } else {
+    currOperand.value = operate[ currOperator ]( prevOperand.value, currOperand.value )
+  }
 }
 
 const attachToHistory = () => {
-	if ( currOperator === '%' ) {
-		history.push( currOperation.value + operate[ currOperator ]( currOperand.value ) )
-	} else {
-		history.push( currOperation.value + operate[ currOperator ]( prevOperand.value, currOperand.value ) )
-	}
+  if ( currOperator === '%' ) {
+    history.push( currOperation.value + operate[ currOperator ]( currOperand.value ) )
+  } else {
+    history.push( currOperation.value + operate[ currOperator ]( prevOperand.value, currOperand.value ) )
+  }
 }
 
 const add = () => {
-	currOperator = '+'
-	hasActiveOperator = true
-	attachToCurrOperation( '+' )
-	setPrevOperand()
+  currOperator = '+'
+  hasActiveOperator = true
+  attachToCurrOperation( '+' )
+  setPrevOperand()
 }
 
 const subtract = () => {
-	currOperator = '-'
-	hasActiveOperator = true
-	attachToCurrOperation( '-' )
-	setPrevOperand()
+  currOperator = '-'
+  hasActiveOperator = true
+  attachToCurrOperation( '-' )
+  setPrevOperand()
 }
 
 const multiply = () => {
-	currOperator = '×'
-	hasActiveOperator = true
-	attachToCurrOperation( '×' )
-	setPrevOperand()
+  currOperator = '×'
+  hasActiveOperator = true
+  attachToCurrOperation( '×' )
+  setPrevOperand()
 }
 
 const divide = () => {
-	currOperator = '÷'
-	hasActiveOperator = true
-	attachToCurrOperation( '÷' )
-	setPrevOperand()
+  currOperator = '÷'
+  hasActiveOperator = true
+  attachToCurrOperation( '÷' )
+  setPrevOperand()
 }
 
 const percent = () => {
-	currOperator = '%'
-	// Will allow for a new operation to start if a digit is pressed instead of an operator.
-	hasActiveOperator = true
-	attachToCurrOperation( '%=' )
-	attachToHistory()
-	setCurrOperand()
-	prevOperand.value = null
+  currOperator = '%'
+  // Will allow for a new operation to start if a digit is pressed instead of an operator.
+  hasActiveOperator = true
+  attachToCurrOperation( '%=' )
+  attachToHistory()
+  setCurrOperand()
+  prevOperand.value = null
 }
 
 const equal = () => {
-	attachToCurrOperation( '=' )
-	attachToHistory()
-	setCurrOperand()
-	prevOperand.value = null
-	// Will allow for a new operation to start if a digit is pressed instead of an operator.
-	hasActiveOperator = true
+  attachToCurrOperation( '=' )
+  attachToHistory()
+  setCurrOperand()
+  prevOperand.value = null
+  // Will allow for a new operation to start if a digit is pressed instead of an operator.
+  hasActiveOperator = true
 }
 
 window.addEventListener( 'keydown', event => {
-	switch ( event.key ) {
-		case '0':
-			document.getElementById( 'button-zero' ).click()
-			break
-		case '1':
-			document.getElementById( 'button-one' ).click()
-			break
-		case '2':
-			document.getElementById( 'button-two' ).click()
-			break
-		case '3':
-			document.getElementById( 'button-three' ).click()
-			break
-		case '4':
-			document.getElementById( 'button-four' ).click()
-			break
-		case '5':
-			document.getElementById( 'button-five' ).click()
-			break
-		case '6':
-			document.getElementById( 'button-six' ).click()
-			break
-		case '7':
-			document.getElementById( 'button-seven' ).click()
-			break
-		case '8':
-			document.getElementById( 'button-eight' ).click()
-			break
-		case '9':
-			document.getElementById( 'button-nine' ).click()
-			break
-		case '.':
-			document.getElementById( 'button-period' ).click()
-			break
-		case '+':
-			document.getElementById( 'button-add' ).click()
-			break
-		case '-':
-			document.getElementById( 'button-subtract' ).click()
-			break
-		case '*':
-			document.getElementById( 'button-multiply' ).click()
-			break
-		case '/':
-			// Prevent Quick Find shortcut in Firefox.
-			event.preventDefault()
-			document.getElementById( 'button-divide' ).click()
-			break
-		case '%':
-			document.getElementById( 'button-percent' ).click()
-			break
-		case 'Enter':
-			document.getElementById( 'button-equal' ).click()
-			break
-		case 'Backspace':
-			document.getElementById( 'button-clear-entry' ).click()
-			break
-		case 'Delete':
-			document.getElementById( 'button-delete' ).click()
-			break
-	}
+  switch ( event.key ) {
+  case '0':
+    document.getElementById( 'button-zero' ).click()
+    break
+  case '1':
+    document.getElementById( 'button-one' ).click()
+    break
+  case '2':
+    document.getElementById( 'button-two' ).click()
+    break
+  case '3':
+    document.getElementById( 'button-three' ).click()
+    break
+  case '4':
+    document.getElementById( 'button-four' ).click()
+    break
+  case '5':
+    document.getElementById( 'button-five' ).click()
+    break
+  case '6':
+    document.getElementById( 'button-six' ).click()
+    break
+  case '7':
+    document.getElementById( 'button-seven' ).click()
+    break
+  case '8':
+    document.getElementById( 'button-eight' ).click()
+    break
+  case '9':
+    document.getElementById( 'button-nine' ).click()
+    break
+  case '.':
+    document.getElementById( 'button-period' ).click()
+    break
+  case '+':
+    document.getElementById( 'button-add' ).click()
+    break
+  case '-':
+    document.getElementById( 'button-subtract' ).click()
+    break
+  case '*':
+    document.getElementById( 'button-multiply' ).click()
+    break
+  case '/':
+    // Prevent Quick Find shortcut in Firefox.
+    event.preventDefault()
+    document.getElementById( 'button-divide' ).click()
+    break
+  case '%':
+    document.getElementById( 'button-percent' ).click()
+    break
+  case 'Enter':
+    document.getElementById( 'button-equal' ).click()
+    break
+  case 'Backspace':
+    document.getElementById( 'button-clear-entry' ).click()
+    break
+  case 'Delete':
+    document.getElementById( 'button-delete' ).click()
+    break
+  }
 })
 </script>
 
 <style scoped lang="scss">
 .calculator {
-	font-size: 1.5rem;
-	color: var(--white);
-	background-color: var(--gray-950);
-	width: 22rem;
-	max-width: 100%;
-	padding: 1rem;
-	margin: 0 auto;
-	display: grid;
-	grid-template-columns: repeat(4, 1fr);
-	grid-gap: 0.25rem;
-	position: relative;
-	overflow: visible;
+  font-size: 1.5rem;
+  color: var(--white);
+  background-color: var(--gray-950);
+  width: 22rem;
+  max-width: 100%;
+  padding: 1rem;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 0.25rem;
+  position: relative;
+  overflow: visible;
 }
 
 .current-operation {
-	grid-column: 1/5;
-	color: var(--gray-400);
-	text-align: right;
-	padding: 0 1rem;
-	min-height: 1.75rem;
+  grid-column: 1/5;
+  color: var(--gray-400);
+  text-align: right;
+  padding: 0 1rem;
+  min-height: 1.75rem;
 }
 
 .display {
-	font-size: 3rem;
-	text-align: right;
-	padding: 0.5rem 1rem 1.5rem;
-	grid-column: 1/5;
+  font-size: 3rem;
+  text-align: right;
+  padding: 0.5rem 1rem 1.5rem;
+  grid-column: 1/5;
 }
 
 .button {
-	line-height: 1;
-	padding: 1em 0;
-	border: none;
-	background-color: var(--gray-900);
-	transition: background-color 0.32s ease, outline 0.32s ease;
-	outline: 2px solid transparent;
-	cursor: pointer;
+  line-height: 1;
+  padding: 1em 0;
+  border: none;
+  background-color: var(--gray-900);
+  transition: background-color 0.32s ease, outline 0.32s ease;
+  outline: 2px solid transparent;
+  cursor: pointer;
 
-	&:focus {
-		outline: 2px solid var(--white);
-	}
+  &:focus {
+    outline: 2px solid var(--white);
+  }
 
-	&:hover {
-		background-color: var(--gray-800);
-	}
+  &:hover {
+    background-color: var(--gray-800);
+  }
 }
 
 .history {
-	grid-column: 1/2;
-	background-color: transparent;
+  grid-column: 1/2;
+  background-color: transparent;
 }
 
 .operator {
-	color: var(--purple-500);
+  color: var(--purple-500);
 
-	&:focus {
-		outline-color: var(--purple-500);
-	}
+  &:focus {
+    outline-color: var(--purple-500);
+  }
 }
 
 .helper {
-	color: var(--teal-500);
+  color: var(--teal-500);
 
-	&:focus {
-		outline-color: var(--teal-500);
-	}
+  &:focus {
+    outline-color: var(--teal-500);
+  }
 }
 </style>
